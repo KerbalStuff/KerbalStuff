8d77f5076777b125b342b4bc75d669084b288c2e
Drew DeVault <sir@cmpwn.com>
2014-07-06 21:31:37 -0600

    Fix addition of new versions

--- KerbalStuff/blueprints/admin.py
+++ KerbalStuff/blueprints/admin.py
@@ -3,7 +3,6 @@ from sqlalchemy import desc
 from KerbalStuff.objects import User, Mod, GameVersion
 from KerbalStuff.database import db
 from KerbalStuff.common import *
-from KerbalStuff.versions import game_versions, load_versions
 
 admin = Blueprint('admin', __name__, template_folder='../../templates/admin')
 
@@ -13,7 +12,8 @@ def backend():
     users = User.query.count()
     new_users = User.query.order_by(desc(User.created)).limit(24)
     mods = Mod.query.count()
-    return render_template("admin.html", users=users, mods=mods, new_users=new_users, versions=game_versions)
+    versions = GameVersion.query.order_by(desc(GameVersion.id)).all()
+    return render_template("admin.html", users=users, mods=mods, new_users=new_users, versions=versions)
 
 @admin.route("/versions/create", methods=['POST'])
 @adminrequired
@@ -22,13 +22,9 @@ def create_version():
     friendly = request.form.get("friendly_version")
     if not friendly:
         return redirect("/asdf")
-    if any([v for v in game_versions if v.friendly_version == friendly]):
+    if any(GameVersion.query.filter(GameVersion.friendly_version == friendly)):
         return redirect("/fsda")
-    print(friendly)
     version = GameVersion(friendly)
-    print(version)
     db.add(version)
     db.commit()
-    print("done")
-    load_versions()
     return redirect("/admin")
--- KerbalStuff/versions.py
+++
@@ -1,6 +0,0 @@
-from KerbalStuff.objects import GameVersion
-
-game_versions = list()
-
-def load_versions():
-    game_versions = [v for v in GameVersion.query.all()]
