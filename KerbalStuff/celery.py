66fe65013a0a0326df4f6d81246f39336777d73e
Thomas <dorianstoll@googlemail.com>
2016-02-16 09:08:27 +0100

    Fixed indents

--- KerbalStuff/celery.py
+++ KerbalStuff/celery.py
@@ -15,14 +15,14 @@ def chunks(l, n):
 def send_mail(sender, recipients, subject, message, important=False):
     if _cfg("smtp-host") == "":
         return
-	smtp = None
-	# If there's a config option provided, use tls
-	if _cfgb('tls'):
-		smtp = smtplib.SMTP_SSL(_cfg("smtp-host"), _cfgi("smtp-port"), keyfile=open(_cfg('tls-keyfile')), certfile=open(_cfg('tls-certfile')))
-	else:		
-		smtp = smtplib.SMTP(host=_cfg("smtp-host"), port=_cfgi("smtp-port"))
-	if not _cfg("smtp-user") == "" and not _cfg("smtp-password") == "":
-		smtp.login(_cfg("smtp-user"), _cfg("smtp-password"))
+    smtp = None
+    # If there's a config option provided, use tls
+    if _cfgb('tls'):
+        smtp = smtplib.SMTP_SSL(_cfg("smtp-host"), _cfgi("smtp-port"), keyfile=open(_cfg('tls-keyfile')), certfile=open(_cfg('tls-certfile')))
+    else:		
+        smtp = smtplib.SMTP(host=_cfg("smtp-host"), port=_cfgi("smtp-port"))
+    if not _cfg("smtp-user") == "" and not _cfg("smtp-password") == "":
+        smtp.login(_cfg("smtp-user"), _cfg("smtp-password"))
     message = MIMEText(message)
     if important:
         message['X-MC-Important'] = "true"
