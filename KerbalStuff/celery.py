a718f70f9a48335a145a6ffae7d1bea0905fe291
Thomas <dorianstoll@googlemail.com>
2016-02-16 09:04:47 +0100

    Fixed issues with SMTP options

--- KerbalStuff/celery.py
+++ KerbalStuff/celery.py
@@ -16,12 +16,12 @@ def send_mail(sender, recipients, subject, message, important=False):
     if _cfg("smtp-host") == "":
         return
 	smtp = None
-	# If there's a config option provided, use starttls
+	# If there's a config option provided, use tls
 	if _cfgb('tls'):
-		smtp = smtplib.SMTP_SSL(_cfg("smtp-host"), _cfgi("smtp-port"), )
+		smtp = smtplib.SMTP_SSL(_cfg("smtp-host"), _cfgi("smtp-port"), keyfile=open(_cfg('tls-keyfile')), certfile=open(_cfg('tls-certfile')))
 	else:		
-		smtp = smtplib.SMTP(host=_cfg("smtp-host"), port=_cfgi("smtp-port"), keyfile=open(_cfg('tls-keyfile')), certfile=open(_cfg('tls-certfile')))
-	if _cfg("smtp-user") == "" and _cfg("smtp-password") == "":
+		smtp = smtplib.SMTP(host=_cfg("smtp-host"), port=_cfgi("smtp-port"))
+	if not _cfg("smtp-user") == "" and not _cfg("smtp-password") == "":
 		smtp.login(_cfg("smtp-user"), _cfg("smtp-password"))
     message = MIMEText(message)
     if important:
