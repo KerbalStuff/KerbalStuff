cf4c84cbaa2918ba0d1d77a40a9f6a0c35b281c5
VITAS <VITAS@52k.de>
2016-04-06 16:40:36 +0200

    removed dark theme from templates and reg

--- KerbalStuff/blueprints/profile.py
+++ KerbalStuff/blueprints/profile.py
@@ -70,7 +70,7 @@ def profile(username):
         profile.backgroundMedia = request.form.get('backgroundMedia')
         bgOffsetX = request.form.get('bg-offset-x')
         bgOffsetY = request.form.get('bg-offset-y')
-        profile.dark_theme = request.form.get('dark_theme')
+        profile.dark_theme = False
         if bgOffsetX:
             profile.bgOffsetX = int(bgOffsetX)
         if bgOffsetY:
