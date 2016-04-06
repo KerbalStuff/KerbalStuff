39d4f945976d9e39f7ea70aa01390b7680162ef1
Christopher Andrews <godarklight@gmail.com>
2016-04-07 01:22:20 +1000

    Prevent crashing if images are missing.

--- KerbalStuff/thumbnail.py
+++ KerbalStuff/thumbnail.py
@@ -1,6 +1,9 @@
+import os.path
 from PIL import Image
 
 def create(imagePath, thumbnailPath, thumbnailSize):
+    if not os.path.isfile(imagePath):
+        return
     im = Image.open(imagePath)
     im.thumbnail(thumbnailSize, Image.ANTIALIAS)
     im.save(thumbnailPath)
