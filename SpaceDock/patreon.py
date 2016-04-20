05d7086838cb593efe3cd4889a9559f3df62d3c3
Christopher Andrews <godarklight@gmail.com>
2016-04-20 10:34:54 +1000

    Fix crash on the first call to the patreon method

--- SpaceDock/patreon.py
+++ SpaceDock/patreon.py
@@ -8,17 +8,20 @@ import json
 donation_cache = redis.Redis(host=_cfg('patreon-host'), port=_cfg('patreon-port'), db=_cfg('patreon-db'))
 
 def GetDonationAmount():
-    last_update_time = float(donation_cache.get('patreon_update_time').decode('utf-8'))
-    donation_amount = int(donation_cache.get('patreon_donation_amount').decode('utf-8'))
+    last_update_time = donation_cache.get('patreon_update_time')
+    donation_amount = donation_cache.get('patreon_donation_amount')
     request_update = False
 
     if donation_amount is None:
         donation_amount = 0
         donation_cache.set('patreon_donation_amount', 0)
+    else:
+        donation_amount = int(donation_amount.decode('utf-8'))
     
     if last_update_time is None:
         request_update = True
     else:
+        last_update_time = float(last_update_time.decode('utf-8'))
         time_delta = time.time() - last_update_time
         #Update every 10 minutes
         if time_delta > 600:
